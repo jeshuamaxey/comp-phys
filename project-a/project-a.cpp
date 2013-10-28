15683db55499a58ce08c7335260e46547072b504
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-10-28 20:58:03 +0000

    euler and leapfrog now work

--- project-a/project-a.cpp
+++ project-a/project-a.cpp
@@ -4,9 +4,11 @@
 #include <cmath>
 #include <ctime>
 
-#define numberOfSteps 5000									//number of time steps
-#define h 0.005														//step size
-#define g 9.81														//acceleration due to gravity
+#define simulatedTime 10												//simulated time (seconds)
+#define h 0.005																//step size
+//#define numberOfSteps  int(simulatedTime / h)	//
+#define g 9.81																//acceleration due to gravity
+#define pi atan(1.0)													//mutha fuckin pi man
 
 using namespace std;
 
@@ -14,13 +16,14 @@ using namespace std;
 
 int main()
 {
+	int numberOfSteps = int(simulatedTime/h);//
 	double t = 0;																//time (needed?)
-	double l = 1.0;															//length of pendulem in metres
+	double l = 9.81;														//length of pendulem in metres
 	double m = 1.0;															//mass of pendulum in kg
-	double gamma = 200.0;												//damping coefficient
+	double gamma = 0.0;													//damping coefficient
 	double beta = gamma/(m* sqrt( g*l ));				//matrix constant
 
-	double initial_theta = 0.3;									//angle from vert (starting angle)
+	double initial_theta = 0.5;									//angle from vert (starting angle)
 	double initial_w = 0;												//rate of change of angle from vert (starting at rest)
 	
 	double euler_theta_n, euler_w_n;						//used to keep track of current values during update calculations
@@ -31,7 +34,8 @@ int main()
 	double leapfrog_theta [numberOfSteps];			//stores all theta values
 	double leapfrog_w [numberOfSteps];					//stores all w values
 
-	
+	std::cout.precision(1);											//sets the number of decimal places time is outputted to
+
 	//set initial values
 	euler_theta[0] = initial_theta;
 	euler_w[0] = initial_w;
@@ -40,24 +44,36 @@ int main()
 
 	//open up file and set column headings
 	ofstream single_pen("data/single_pen.csv");
-	single_pen << "time,euler_theta,leapfrog_theta" << endl; //euler only for now
-	//single_pen << "time,euler_theta,euler_w,leapfrog_theta,leapfrog_w" << endl;
+	//first column is always time
+	single_pen << "time" ;
+	//euler
+	single_pen << ",euler_theta,euler_w" ;
+	//leapfrog
+	single_pen << ",leapfrog_theta,leapfrog_w" ;
+	//end column headers
+	single_pen << "\n";
 
 	for (int i = 0; i < numberOfSteps; i++)
 	{
 		/************** OUTPUT **************/
-
-		//output results to file
-		//single_pen << i*h << "," << euler_theta[i] << "," << euler_w[i] << "," << leapfrog_theta[i] << "," << leapfrog_w[i] << endl;
-		single_pen << i*h << "," << euler_theta[i] << "," << leapfrog_theta[i] << endl;
+		//output time
+		single_pen << std::fixed << h*i ;
+		//output euler
+		single_pen << std::scientific << "," << euler_theta[i] << "," << euler_w[i] ;
+		//output leapfrog
+		single_pen << std::scientific << "," << leapfrog_theta[i] << "," << leapfrog_w[i] ;
+		//end output for this iteration
+		single_pen << "\n";
+		
 		/************** EULER METHOD **************/
 
 		//update euler_theta
 		euler_theta[i+1] =  ( h*euler_w[i] ) + euler_theta[i];
 		//update euler_w
-		euler_w[i+1] = ( (1-h) * euler_w[i] ) - ( h * beta * euler_theta[i] );
+		//euler_w[i+1] = ( (1-h) * euler_w[i] ) - ( h * beta * euler_theta[i] );		//bad maths
+		euler_w[i+1] = -h*euler_theta[i] + ( 1 - h*beta )*euler_w[i];						//good maths
 
-		/************** LEAPFROG METHOD ************** /
+		/************** LEAPFROG METHOD **************/
 
 		if(i==0) //we need to use the euler method to give us some initial values
 		{
@@ -69,18 +85,13 @@ int main()
 		else
 		{
 			//update leapfrog_theta
-			leapfrog_theta[i+1] =  leapfrog_theta[i-1] + leapfrog_w[i]*2*h;
+			leapfrog_theta[i+1] =  leapfrog_theta[i-1] + leapfrog_w[i]*2.0*h;
 			//update leapfrog_w
-			leapfrog_w[i+1] = leapfrog_w[i-1] - ( leapfrog_w[i] + ( beta*leapfrog_theta[i] )*2*h );
-		}
-
-		if(i>400)
-		{
-			break;
+			leapfrog_w[i+1] = leapfrog_w[i-1] - 2.0*h*( leapfrog_theta[i] + ( beta*leapfrog_w[i] ) );
 		}
 
 		/************** PROGRESS METER **************/
-		cout << "\r"<< (float(i)/numberOfSteps)*100 << "\%" << flush;
+		cout << std::fixed<< "\r"<< (float(i)/numberOfSteps)*100 << "\%" << flush;
 
 	}
 
