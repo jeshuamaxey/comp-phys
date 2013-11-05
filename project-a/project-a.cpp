13a843cd8fbf5e843c645acaad480e931f77c363
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-11-05 21:59:10 +0000

    refactored the outputting to files and added bool options to output certain params only

--- project-a/project-a.cpp
+++ project-a/project-a.cpp
@@ -4,52 +4,75 @@
 #include <cmath>
 #include <ctime>
 
-#define simulatedTime 30											//simulated time (seconds)
-#define h 0.005																//step size
+#define simulatedTime 5											//simulated time (seconds)
+#define h 0.1																//step size
 #define numberOfSteps  int(simulatedTime / h)	//used for sizing arrays
 #define g 9.81																//acceleration due to gravity
 #define pi atan(1.0)													//mutha fuckin pi man
 
+#define outputPositions true									//determine what's outputted by the program to file
+#define outputEnergies false									//determine what's outputted by the program to file
+
 using namespace std;
 
-//function prototypes
-//void outputToFile(ostream&);								//not used
+
+//single pendulum functions
 void single_pendulum();
+double calculateKineticEnergy(double m, double l, double theta, double w);
+double calculatePotentialEnergy(double m, double l, double theta, double w);
+
+//double pendulum functions
 void double_pendulum();
+
+//helper functions
 void updateProgress(int);
 void updateProgress(int, char[]);
+void done();
 
-int main() {
+int main()
+{
 	single_pendulum();
 	double_pendulum();
-	/************** PROGRESS METER **************/
-	std::cout << "\r"<< "100\% - all done. Simulated time elapsed: " << simulatedTime << "s\n" << std::flush;
+	done();
 	return 0;
 }
 
+//simulates the motion of a single pendulum
+//using multiple finite difference methods
 void single_pendulum()
 {
 	char processName[64] = "Single Pendulum";
 
-	double t = 0;																//time (needed?)
+	double t = 0;																//time
 	double l = 9.81;														//length of pendulem in metres
 	double m = 1.0;															//mass of pendulum in kg
-	double gamma = 1.0;													//damping coefficient
+	double gamma = 0.0;													//damping coefficient
 	double beta = gamma/(m* sqrt( g*l ));				//matrix constant
 
+	//initial conditions
 	double initial_theta = 0.5;									//angle from vert (starting angle)
 	double initial_w = 0;												//rate of change of angle from vert (starting at rest)
 	
-	double euler_theta_n, euler_w_n;						//used to keep track of current values during update calculations
+	//euler variable arrays
 	double euler_theta [numberOfSteps];					//stores all theta values
 	double euler_w [numberOfSteps];							//stores all w values
+	double euler_E [numberOfSteps];							//stores all E values
+	double euler_T [numberOfSteps];							//stores all T values
+	double euler_U [numberOfSteps];							//stores all U values
 
-	double leapfrog_theta_n, leapfrog_w_n;			//used to keep track of current values during update calculations
+	//leapfrog variable arrays
 	double leapfrog_theta [numberOfSteps];			//stores all theta values
 	double leapfrog_w [numberOfSteps];					//stores all w values
+	double leapfrog_E [numberOfSteps];					//stores all E values
+	double leapfrog_T [numberOfSteps];					//stores all T values
+	double leapfrog_U [numberOfSteps];					//stores all U values
 
-	double rk4_theta [numberOfSteps];
-	double rk4_w [numberOfSteps];
+	//rk4 variable arrays
+	double rk4_theta [numberOfSteps];						//stores all theta values
+	double rk4_w [numberOfSteps];								//stores all w values
+	double rk4_E [numberOfSteps];								//stores all E values
+	double rk4_T [numberOfSteps];								//stores all T values
+	double rk4_U [numberOfSteps];								//stores all U values
 	double k_1, k_2, k_3, k_4;
 
 	std::cout.precision(1);											//sets the number of decimal places time is outputted to
@@ -58,13 +81,17 @@ void single_pendulum()
 	//set initial values
 	euler_theta[0] = initial_theta;
 	euler_w[0] = initial_w;
+
 	leapfrog_theta[0] = initial_theta;
 	leapfrog_w[0] = initial_w;
+	
 	rk4_theta[0] = initial_theta;
 	rk4_w[0] = initial_w;
 
+
+
 	//open up file and set column headings
-	ofstream single_pen("data/single_pen.csv");
+	ofstream single_pen("data/single_pen_combined.csv");
 	//first column is always time
 	single_pen << "time" ;
 	//euler
@@ -76,11 +103,84 @@ void single_pendulum()
 	//end column headers
 	single_pen << "\n";
 
+	//open up file and set column headings
+
+	ofstream euler_single_pen("data/euler_single_pen.csv");
+	ofstream leapfrog_single_pen("data/leapfrog_single_pen.csv");
+	ofstream rk4_single_pen("data/rk4_single_pen.csv");
+
+	//first column is always time
+	euler_single_pen << "time" ;
+	leapfrog_single_pen << "time" ;
+	rk4_single_pen << "time" ;
+	
+	if(outputPositions)
+	{
+		//position column headings
+		euler_single_pen << ",euler_theta,euler_w" ;
+		leapfrog_single_pen << ",leapfrog_theta,leapfrog_w" ;
+		rk4_single_pen << ",rk4_theta,rk4_w" ;
+	}
+	if(outputEnergies)
+	{
+		//energies column headings
+		euler_single_pen << ",euler_T,euler_U, euler_E" ;
+		leapfrog_single_pen << ",leapfrog_T,leapfrog_U, leapfrog_E" ;
+		rk4_single_pen << ",rk4_T,rk4_U, rk4_E" ;
+	}
+
+	//end column headings
+	euler_single_pen << "\n";
+	leapfrog_single_pen << "\n";
+	rk4_single_pen << "\n";
+
 	for (int i = 0; i < numberOfSteps; i++)
 	{
-		/************** OUTPUT **************/
+		/************** ENERGIES **************/
+
+		//euler
+		euler_T[i] = calculateKineticEnergy(m, l, euler_theta[i], euler_w[i]);
+		euler_U[i] = calculatePotentialEnergy(m, l, euler_theta[i], euler_w[i]);
+		euler_E[i] = euler_T[i] + euler_U[i];
+
+		//leapfrog
+		leapfrog_T[i] = calculateKineticEnergy(m, l, leapfrog_theta[i], leapfrog_w[i]);
+		leapfrog_U[i] = calculatePotentialEnergy(m, l, leapfrog_theta[i], leapfrog_w[i]);
+		leapfrog_E[i] = leapfrog_T[i] + leapfrog_U[i];
+
+		//RK4
+		rk4_T[i] = calculateKineticEnergy(m, l, rk4_theta[i], rk4_w[i]);
+		rk4_U[i] = calculatePotentialEnergy(m, l, rk4_theta[i], rk4_w[i]);
+		rk4_E[i] = rk4_T[i] + rk4_U[i];
+
+		/************** OUTPUT TO FILE **************/
+		
 		//output time
-		single_pen << std::fixed << h*i ;
+		t = h*i;
+		euler_single_pen 		<< std::fixed << t ;
+		leapfrog_single_pen << std::fixed << t ;
+		rk4_single_pen 			<< std::fixed << t ;
+		
+		//positions
+		if(outputPositions)
+		{
+			euler_single_pen << std::scientific << "," << euler_theta[i] << "," << euler_w[i] ;
+			leapfrog_single_pen << std::scientific << "," << leapfrog_theta[i] << "," << leapfrog_w[i] ;
+			rk4_single_pen << std::scientific << "," << rk4_theta[i] << "," << rk4_w[i] ;
+		}
+		//energies
+		if(outputEnergies)
+		{
+			euler_single_pen << std::scientific << "," << euler_T[i] << "," << euler_U[i] << "," << euler_E[i];
+			leapfrog_single_pen << std::scientific << "," << leapfrog_T[i] << "," << leapfrog_U[i] << "," << leapfrog_E[i];
+			rk4_single_pen << std::scientific << "," << rk4_T[i] << "," << rk4_U[i] << "," << rk4_E[i];
+		}
+		euler_single_pen << "\n" ;
+		leapfrog_single_pen << "\n" ;
+		rk4_single_pen << "\n" ;
+
+		//output time
+		single_pen << std::fixed << t ;
 		//output euler
 		single_pen << std::scientific << "," << euler_theta[i] << "," << euler_w[i] ;
 		//output leapfrog
@@ -130,11 +230,14 @@ void single_pendulum()
 
 		rk4_w[i+1] = rk4_w[i] + (1.0/6.0)*(k_1 + 2*k_2 + 2*k_3 + k_4);
 
+		//update progress meter in terminal
 		updateProgress(i, processName);
 
 	}
 } //end single_pendulum()
 
+//simulates the motion of a double pendulum
+//using the RK4 finite difference method
 void double_pendulum()
 {
 	char processName[64] = "Double Pendulum";
@@ -212,7 +315,7 @@ void double_pendulum()
 		k_3 = h * ( -1*(R+1)*(rk4_theta[i] + 0.5*h) + R*(rk4_psi[i] + 0.5*h)  - beta*(rk4_w[i] + 0.5*h) );
 		k_4 = h * ( -1*(R+1)*(rk4_theta[i] + h) 		+ R*(rk4_psi[i] + h) 			- beta*(rk4_w[i] + h) );
 		//generate next value of w
-		rk4_w[i+1] = rk4_w[i] + (1.0/6.0)*(k_1 + 2*k_2 + 2*k_3 + k_4);
+		rk4_w[i+1] = rk4_w[i] + (1.0/6.0)*(k_1 + 2*k_2 + 2*k_3 + k_4);						//DONE
 		
 		//generate the k values for v
 		k_1 = h * ( (R+1)*rk4_theta[i] 						- (R+1)*rk4_psi[i] 					 + beta*(1- (1/R))*rk4_w[i] 					- (beta/R)*rk4_v[i] );
@@ -228,7 +331,26 @@ void double_pendulum()
 	}
 } //end double_pendulum
 
+//calculate kinetic energy
+double calculateKineticEnergy(double m, double l, double theta, double w)
+{
+	return 0.5*m*pow(l,2.0)*pow(w,2.0);
+}
+
+//calculate potential energy
+double calculatePotentialEnergy(double m, double l, double theta, double w)
+{
+	return m*g*l*(1 - cos(theta));
+}
+
 //updates the progress display on commandline
-void updateProgress(int i, char *name) {
+void updateProgress(int i, char *name)
+{
 		cout << std::fixed << "\r"<< "Currently running " << name << " - " << (float(i)/numberOfSteps)*100 << "\%" << flush;
+}
+
+//
+void done()
+{
+	std::cout << "\r"<< "100\% - all done. Simulated time elapsed: " << simulatedTime << "s\n" << std::flush;
 }
