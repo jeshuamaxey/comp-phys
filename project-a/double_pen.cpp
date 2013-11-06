b76745970a96f553dd7734362cf6a91b4796ed86
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-11-06 22:44:25 +0000

    refactoring has gone like a dream. Need to add a data-files.txt output file which lists all data files produced for use with data viewer next then implement crazy loops

--- project-a/double_pen.cpp
+++ project-a/double_pen.cpp
@@ -3,10 +3,13 @@
 #include <fstream>
 #include <cmath>
 #include <ctime>
+#include <sstream>
+#include <string>
 
 #define simulatedTime 60.0										//simulated time (seconds)
-//#define h 0.01																//step size
-//#define numberOfSteps  int(simulatedTime / h)	//used for sizing arrays
+#define h_min 0.01
+#define h_max 0.5
+#define numberOfSteps int(simulatedTime / h_min)	//used for sizing arrays
 #define g 9.81																//acceleration due to gravity
 #define pi atan(1.0)													//mutha fuckin pi man
 
@@ -16,8 +19,9 @@
 using namespace std;
 
 //double pendulum functions
-void double_pendulum();
+void double_pendulum(double, double, double);
 void updateRK4(double, double, double, int);
+string makeFileName(double h, double G, double R);
 
 //helper functions
 void updateProgress(int);
@@ -26,7 +30,10 @@ void done();
 
 int main()
 {
-	double_pendulum();
+	double h = h_min;														//step size
+	double G = 0;																//matrix constant - gamma/(m* sqrt( g*l ))
+	double R = 100;															//matrix constant - M/m
+	double_pendulum(h, G, R);
 	done();
 	return 0;
 }
@@ -67,31 +74,14 @@ double k1[4], k2[4], k3[4], k4[4];					//RK4 values
 
 //simulates the motion of a double pendulum
 //using the RK4 finite difference method
-void double_pendulum()
+void double_pendulum(double h, double G, double R)
 {
-	double h =  0.01;																//step size
-	int numberOfSteps = int(simulatedTime / h);		//used for sizing arrays
-
 	char processName[64] = "Double Pendulum";
-
-	double t = 0;																//time (needed?)
-	double l = 9.81;														//length of pendulem in metres
-	double m = 1.0;															//mass of 1st pendulum in kg
-	double M = 1.0;															//mass of 2nd pendulum in kg
-	double gamma = 0.0;													//damping coefficient
-
-	double beta = gamma/(m* sqrt( g*l ));				//matrix constant
-	double G = beta;														//matrix constant
-	double R = M/m;															//matrix constant
-
-	R = 100;
 
 	double initial_theta = 0.1;									//angle from vert for pendulum 1 (starting angle)
 	double initial_psi = 0.0;										//angle from vert for pendulum 2 (starting angle)
 	double initial_w = 0.0;											//rate of change of angle from vert for pendulum 1 (starting at rest)
 	double initial_v = 0.0;											//rate of change of angle from vert for pendulum 2 (starting at rest)
-
-
 
 	std::cout.precision(1);											//sets the number of decimal places time is outputted to
 																							//see: http://www.cplusplus.com/reference/ios/scientific/
@@ -102,8 +92,9 @@ void double_pendulum()
 	rk4_w[0] = initial_w;
 	rk4_v[0] = initial_v;
 
+	string fileName = makeFileName(h, G, R);
 	//open up file and set column headings
-	ofstream double_pen("data/double_pen.csv");
+	ofstream double_pen(fileName);
 	//first column is always time
 	double_pen << "time" ;
 	//rk4
@@ -121,42 +112,15 @@ void double_pendulum()
 		//end output for this iteration
 		double_pen << "\n";
 
-		updateRK4(h, R, G);
+		updateRK4(h, R, G, i);
 
-		/************** NEW RK4 METHOD ************** /
-
-		k1[0] = h * rk4_w[i];
-		k1[1] = h * rk4_v[i];
-		k1[2] = h * ( -(R+1.0)*rk4_theta[i] + R*rk4_psi[i] - G*rk4_w[i] );
-		k1[3] = h * ( (R+1.0)*rk4_theta[i] -(R+1.0)*rk4_psi[i] + G*(1.0-(1.0/R))*rk4_w[i] - (G/R)*rk4_v[i] );
-
-		k2[0] = h * ( rk4_w[i] 		+ 0.5*k1[2] );
-		k2[1] = h * ( rk4_v[i]	+ 0.5*k1[3] );
-		k2[2] = h * ( -(R+1.0)*(rk4_theta[i] + 0.5*k1[0]) + R*(rk4_psi[i] + 0.5*k1[1]) - G*(rk4_w[i] + 0.5*k1[2]) );
-		k2[3] = h * ( (R+1.0)*(rk4_theta[i] + 0.5*k1[0]) -(R+1.0)*(rk4_psi[i] + 0.5*k1[1]) + G*(1.0-(1.0/R))*(rk4_w[i] + 0.5*k1[2]) - (G/R)*(rk4_v[i] + 0.5*k1[3]) );
-
-		k3[0] = h * ( rk4_w[i] 		+ 0.5*k2[2] );
-		k3[1] = h * ( rk4_v[i]	+ 0.5*k2[3] );
-		k3[2] = h * ( -(R+1.0)*(rk4_theta[i] + 0.5*k2[0]) + R*(rk4_psi[i] + 0.5*k2[1]) - G*(rk4_w[i] + 0.5*k2[2]) );
-		k3[3] = h * ( (R+1.0)*(rk4_theta[i] + 0.5*k2[0]) -(R+1.0)*(rk4_psi[i] + 0.5*k2[1]) + G*(1.0-(1.0/R))*(rk4_w[i] + 0.5*k2[2]) - (G/R)*(rk4_v[i] + 0.5*k2[3]) );
-
-		k4[0] = h * ( rk4_w[i] 		+ k3[2] );
-		k4[1] = h * ( rk4_v[i]	+ k3[3] );
-		k4[2] = h * ( -(R+1.0)*(rk4_theta[i] + k3[0]) + R*(rk4_psi[i] + k3[1]) - G*(rk4_w[i] + k3[2]) );
-		k4[3] = h * ( (R+1.0)*(rk4_theta[i] + k3[0]) -(R+1.0)*(rk4_psi[i] + k3[1]) + G*(1.0-(1.0/R))*(rk4_w[i] + k3[2]) - (G/R)*(rk4_v[i] + k3[3]) );
-
-		rk4_theta[i+1]	= rk4_theta[i]	+ (1.0/6.0)*(k1[0] + 2*k2[0] + 2*k3[0] + k4[0]);
-		rk4_psi[i+1]		= rk4_psi[i]		+ (1.0/6.0)*(k1[1] + 2*k2[1] + 2*k3[1] + k4[1]);
-		rk4_w[i+1]			= rk4_w[i]			+ (1.0/6.0)*(k1[2] + 2*k2[2] + 2*k3[2] + k4[2]);
-		rk4_v[i+1]			= rk4_v[i]			+ (1.0/6.0)*(k1[3] + 2*k2[3] + 2*k3[3] + k4[3]);
-		
 		/************** PROGRESS **************/
 		updateProgress(i, processName);
 
 	}
 } //end double_pendulum
 
-void updateRK4(double h, double R, double G)
+void updateRK4(double h, double R, double G, int i)
 {
 	/************** NEW RK4 METHOD **************/
 	k1[0] = h * rk4_w[i];
@@ -199,6 +163,13 @@ void updateRK4(double h, double R, double G)
                           88                                           
                           88 
 */
+
+string makeFileName(double h, double G, double R)
+{
+	stringstream name;
+	name << "data/double_pen_h="<< h << "_G=" << G << "_R=" << R << ".csv";
+	return name.str();
+}
 
 //updates the progress display on commandline
 void updateProgress(int i, char *name)
