a91c24a7a74c6865f9db4f633de0fc2247b7776c
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-11-21 21:00:56 +0000

    updated the alias file for project-b

--- project-b/aliases.sh
+++ project-b/aliases.sh
@@ -1,1 +1,1 @@
-alias go="g++ project-b.cpp  -lgsl -o project-b && ./project-b && node update-file-list.js"
+alias go="echo compiling project-b.cpp && g++ project-b.cpp  -lgsl -o project-b && echo compilation complete. Running project-b now... && ./project-b && node update-file-list.js"
