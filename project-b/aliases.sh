80ac8160f70e49ab04661e80964fccec5396523c
jeshuamaxey <me@jeshua.co>
2013-12-10 22:53:22 +0000

    committing to get latest code

--- project-b/aliases.sh
+++ project-b/aliases.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 alias go="echo compiling project-b.cpp && g++ project-b.cpp  -lgsl -o project-b && echo compilation complete. Running project-b now... && ./project-b && node update-file-list.js"
